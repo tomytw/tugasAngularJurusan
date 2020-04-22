ac67658e20a4f086579aa91683522bf8c96c8ca1
tomytw <c14180006@john.petra.ac.id>
2020-04-22 09:40:29 +0700

    3 files modified, 6 added - menambah 3 routing halaman

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Tomy';
+
+  constructor(private router : Router) {}
+
+  goInput(){
+    this.router.navigate('/input');
+  }
+
+
 }
